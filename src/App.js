c2099dfc3cb57830d1c74459a34cb0c0a9fd4ab6
Pierrelui <115804391+Pierrelui@users.noreply.github.com>
2025-06-13 11:59:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import './App.css';
 import BusinessList from './componets/BusinessList/BusinessList';
@@ -8,14 +7,18 @@ import businessData from './mockData';
 function App() {
   const [businesses, setBusinesses] = useState(businessData);
 
-  const handleSearchResults = (newBusinessArray) => {
-    setBusinesses(newBusinessArray);
+  // This will be called by SearchBar
+  const searchYelp = (term, location, sortBy) => {
+    console.log('Searching Yelp with:', term, location, sortBy);
+    // For now, just load mock data
+    setBusinesses(businessData);
   };
+
 
   return (
     <div className="App">
       <h1>Ravenous</h1>
-      <SearchBar onSearchResults = {handleSearchResults}/>
+      <SearchBar onSearch = {searchYelp}/>
       <BusinessList businesses={businesses}/>
     </div>
   );
